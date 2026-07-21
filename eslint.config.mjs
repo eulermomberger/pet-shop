4845224b3fec3bce7ae708ac789ae8f967023ffb
euler <mombergereuler@gmail.com>
2026-07-21 20:44:01 -0300

    🔧 chore: Configurando Eslint, Prettier e Lefthook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    'src/generated/prisma/**',
+    'pgdata/**',
+    '.next/**',
+    'node_modules/**',
   ]),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
 ]);
 
 export default eslintConfig;
